1b1dc910d8589c45195c574b0a92d466f47f41bd
YadaShreya25 <125191551+YadaShreya25@users.noreply.github.com>
2023-07-08 23:58:47 +0530

    Time: 16 ms (44.42%), Space: 19.1 MB (11.55%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -15,7 +15,7 @@ public:
         }
         return n.size();
         */
-        int i=0,j=1;
+        /*int i=0,j=1;
         while(j<n.size())
         {
             if(n[i]!=n[j] )
@@ -26,5 +26,17 @@ public:
             j++;
         }
         return i+1;
+        */
+        int j=0;
+        set<int>s;
+        for(int i=0;i<n.size();i++)
+        {
+            s.insert(n[i]);
+        }
+        for(auto x:s)
+        {
+            n[j++]=x;
+        }
+        return s.size();
     }
 };
