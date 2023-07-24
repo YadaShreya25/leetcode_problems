c32a44fc6dcda5aa34bc5319a05c6cf7c30abbd4
YadaShreya25 <125191551+YadaShreya25@users.noreply.github.com>
2023-07-25 00:17:57 +0530

    Time: 58 ms (34.05%), Space: 19.6 MB (78.75%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -1,7 +1,23 @@
 class Solution {
 public:
+    bool check(string &s,int i,int j)
+        {
+            while(i<=j)
+            {
+                if(s[i]==s[j])
+                {
+                    i++;
+                    j--;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+       }
     bool validPalindrome(string s) {
-        int i=0,j=s.size()-1,count1=0,count2=0;
+        int i=0,j=s.size()-1;
         while(i<=j)
         {
             if(s[i]==s[j])
@@ -11,24 +27,9 @@ public:
             }
             else
             {
-                i++;
-                count1++;
+                break;
             }
         }
-        i=0,j=s.size()-1;
-        while(i<=j)
-        {
-            if(s[i]==s[j])
-            {
-                i++;
-                j--;
-            }
-            else
-            {
-                j--;
-                count2++;
-            }
-        }
-        return (count1<2 || count2<2)?true:false;
+        return check(s,i+1,j)|| check(s,i,j-1);
     }
 };
