b205ec2378577774a50cbf9853027bea3de15891
YadaShreya25 <125191551+YadaShreya25@users.noreply.github.com>
2023-07-25 00:09:38 +0530

    Time: 67 ms (6.79%), Space: 19.5 MB (91.76%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool validPalindrome(string s) {
+        int i=0,j=s.size()-1,count1=0,count2=0;
+        while(i<=j)
+        {
+            if(s[i]==s[j])
+            {
+                i++;
+                j--;
+            }
+            else
+            {
+                i++;
+                count1++;
+            }
+        }
+        i=0,j=s.size()-1;
+        while(i<=j)
+        {
+            if(s[i]==s[j])
+            {
+                i++;
+                j--;
+            }
+            else
+            {
+                j--;
+                count2++;
+            }
+        }
+        return (count1<2 || count2<2)?true:false;
+    }
+};
