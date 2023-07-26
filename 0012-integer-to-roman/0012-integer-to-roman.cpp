146b09182286e82a31ad766a466f754324baab91
YadaShreya25 <125191551+YadaShreya25@users.noreply.github.com>
2023-07-26 19:53:04 +0530

    Time: 7 ms (77.51%), Space: 9.3 MB (24.85%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -5,7 +5,7 @@ public:
         string romanValue;
         vector<pair<int,string>>val={{1000,"M"},{900,"CM"},{500,"D"},{400,"CD"},{100,"C"},{90,"XC"},{50,"L"},{40,"XL"},{10,"X"},{9,"IX"},{5,"V"},{4,"IV"},{1,"I"}};
         
-        for(int i=0;n>0;i++)
+        for(int i=0;i<val.size();i++)
         {
             while(n>=val[i].first)
             {
