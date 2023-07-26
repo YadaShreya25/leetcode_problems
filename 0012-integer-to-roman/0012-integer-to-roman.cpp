0404ac6d3b1f6af385bed2a850c5a16cab562c28
YadaShreya25 <125191551+YadaShreya25@users.noreply.github.com>
2023-07-27 00:11:15 +0530

    Time: 11 ms (51.34%), Space: 6 MB (69.49%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -3,17 +3,12 @@ public:
     string intToRoman(int n) {
      
         string romanValue;
-        vector<pair<int,string>>val={{1000,"M"},{900,"CM"},{500,"D"},{400,"CD"},{100,"C"},{90,"XC"},{50,"L"},{40,"XL"},{10,"X"},{9,"IX"},{5,"V"},{4,"IV"},{1,"I"}};
+        string ones[]={"","I","II","III","IV","V","VI","VII","VIII","IX"};
+        string tens[]={"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+        string huns[]={"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+        string thos[]={"","M","MM","MMM"};
         
-        for(int i=0;i<val.size();i++)
-        {
-            while(n>=val[i].first)
-            {
-                romanValue+=val[i].second;
-                n-=val[i].first;
-            }
-        }
-        
+        romanValue=thos[n/1000]+huns[(n%1000)/100]+tens[(n%100)/10]+ones[(n%10)];
         return romanValue;
         
     }
