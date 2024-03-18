ce7dde1d3da1ac194a648a191cc96a5459ec1836
YadaShreya25 <125191551+YadaShreya25@users.noreply.github.com>
2024-03-18 12:30:58 +0530

    Time: 1185 ms (17.21%), Space: 0B (100.00%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -13,22 +13,21 @@ where id not in (select id from Customer where referee_id=2);
 */
 
 
-/*
 #Approach - 3
 select name from Customer
-where coalesce(referee_id,0)!=2;
-*/
+where ifnull(referee_id,0)!=2;
 
 
 
+/*
 #Approach - 4
 select name from Customer
 where case
         when referee_id!=2 then 1
         when referee_id is null then 1
+        #optional to keep else i.e else 0 if it is not present also it gives false
         end;
-
-
+*/
 
 
 
