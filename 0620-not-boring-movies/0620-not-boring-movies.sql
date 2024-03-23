a4644fd941234de44429a54aec84017916f50ad4
YadaShreya25 <125191551+YadaShreya25@users.noreply.github.com>
2024-03-23 23:28:09 +0530

    Time: 811 ms (5.17%), Space: 0B (100.00%) - LeetHub

--- 0620-not-boring-movies/0620-not-boring-movies.sql
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -1,4 +1,17 @@
 # Write your MySQL query statement below
+#Approach - 1
+/*
 select * from Cinema 
 where id%2!=0 && description!='boring'
 order by rating desc;
+*/
+
+# not equal  ( != , <> )
+# and ( and , && )
+# for id odd  ( id%2=1  , id%2!=0, id%2<>0 , mod(id,2)<>0 , mod(id,2)=1 -- Aggregate function mod() )
+
+
+#Approach - 2
+select * from Cinema
+where id % 2 = 1 and description not like "%boring%"
+order by rating desc; 
