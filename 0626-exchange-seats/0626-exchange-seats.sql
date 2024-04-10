1699634d3cef23a70dcb435e5d95acb07d594fc9
YadaShreya25 <125191551+YadaShreya25@users.noreply.github.com>
2024-04-11 00:26:41 +0530

    Time: 781 ms (34.81%), Space: 0B (100.00%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 select case
-       when mod(id,2)=0 then id-1
+       when id % 2 = 0 then id-1
        when id=(select count(id) from Seat) then id
        else id+1
        end as id,student
