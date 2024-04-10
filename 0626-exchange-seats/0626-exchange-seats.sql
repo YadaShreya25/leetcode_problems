d214fe881fcae616adb744684e3e66e5b5d103dc
YadaShreya25 <125191551+YadaShreya25@users.noreply.github.com>
2024-04-11 00:27:06 +0530

    Time: 779 ms (35.23%), Space: 0B (100.00%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
 select case
        when id % 2 = 0 then id-1
-       when id=(select count(id) from Seat) then id
+       when id=(select max(id) from Seat) then id
        else id+1
        end as id,student
        from Seat
