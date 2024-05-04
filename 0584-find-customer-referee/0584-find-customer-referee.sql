df299d37588f7e7a2434a72472323b32c11398ec
YadaShreya25 <125191551+YadaShreya25@users.noreply.github.com>
2024-05-04 22:31:57 +0530

    Time: 1165 ms (24.07%), Space: 0B (100.00%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -2,7 +2,7 @@
 /*
 #Approach - 1 
 select name from Customer 
-where referee_id is null or referee_id!=2;\
+where referee_id is null or referee_id!=2;
 */
 
 
@@ -13,21 +13,63 @@ where id not in (select id from Customer where referee_id=2);
 */
 
 
+/*
 #Approach - 3
 select name from Customer
-where ifnull(referee_id,0)!=2;
-
+where coalesce(referee_id,0)!=2;
+*/
 
 
 /*
 #Approach - 4
 select name from Customer
+where ifnull(referee_id,0)!=2;
+*/
+
+
+
+#Approach - 5
+select name from Customer
 where case
         when referee_id!=2 then 1
         when referee_id is null then 1
-        #optional to keep else i.e else 0 if it is not present also it gives false
+        #optional to keep else i.e else 0 if it is not present also it gives false automatically
         end;
-*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
